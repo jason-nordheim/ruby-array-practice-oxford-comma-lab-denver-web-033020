9f6ab063c583d4e8d70b24dca516f503334a0688
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-07 16:20:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,8 @@ def oxford_comma(array)
   if array.size == 1
     array[0]
   else
-    index = 0
-    until index == array.size - 2
+    while array.size > 2 
       
-    end
+    end 
   end
 end
