9553218644bccbc4ae493941212ffd7924bb700d
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-07 16:24:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   output = ""
   if array.size == 1
     output = array[0]
+  elsif array.size == 2
+    output += "#{array[0]}, and #{array[1]}"
   else
     while array.size > 2
       output += "#{array.shift()}, "
